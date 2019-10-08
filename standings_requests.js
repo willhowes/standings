a9259ac30d18f26d773a65465776acabd05674cc
Kaja <catcalledkaja@gmail.com>
2019-10-08 11:15:57 +0100

    adding name

--- standings_requests.js
+++ standings_requests.js
@@ -4,7 +4,7 @@ axios.all([
   axios.get('https://makers.sd.dazn-dev.com/api/competitions')
 ]).then(axios.spread((response1) => {
 
-  console.log(response1)
+  console.log(response1.data.competition[0].name)
 })).catch(error => {
   console.log(error);
 });
