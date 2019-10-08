8686446e934163da99a507c23bd401d6c540ca30
Kaja <catcalledkaja@gmail.com>
2019-10-08 11:26:04 +0100

    adding id

--- standings_requests.js
+++ standings_requests.js
@@ -3,7 +3,10 @@ const axios = require('axios')
 async function main() {
   try {
     const response1 = await axios.get('https://makers.sd.dazn-dev.com/api/competitions');
+    const id = response1.data.competition[0].id
+    const response2 = await axios.get(`https://makers.sd.dazn-dev.com/api/competitions/${id}`);
     console.log(response1.data.competition[0].name);
+    console.log(response2.data.competitionId)
   } catch(error) {
     console.log(error);
   }
