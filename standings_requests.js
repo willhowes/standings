26e1d51ec1d26fd7d46de8d7db666a3075ac8107
Kaja <catcalledkaja@gmail.com>
2019-10-08 11:18:31 +0100

    chore: refactor to use async

--- standings_requests.js
+++ standings_requests.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
 
-axios.all([
-  axios.get('https://makers.sd.dazn-dev.com/api/competitions')
-]).then(axios.spread((response1) => {
+async function main() {
+  try {
+    const response1 = await axios.get('https://makers.sd.dazn-dev.com/api/competitions');
+    console.log(response1.data.competition[0].name);
+  } catch(error) {
+    console.log(error);
+  }
+}
 
-  console.log(response1.data.competition[0].name)
-})).catch(error => {
-  console.log(error);
-});
+main();
