0b2bea79b4e886883957b5a53ba719ab445596ed
Kaja <catcalledkaja@gmail.com>
2019-10-08 12:00:06 +0100

    returns one object

--- standings_requests.js
+++ standings_requests.js
@@ -1,15 +1,25 @@
 const axios = require('axios')
+const Koa = require('koa');
+const app = new Koa();
 
 async function main() {
   try {
     const response1 = await axios.get('https://makers.sd.dazn-dev.com/api/competitions');
     const id = response1.data.competition[0].id
     const response2 = await axios.get(`https://makers.sd.dazn-dev.com/api/competitions/${id}`);
-    console.log(response1.data.competition[0].name);
-    console.log(response2.data.competitionId)
+    return [{"name": response1.data.competition[0].name, "standings": response2.data}]
+    
   } catch(error) {
     console.log(error);
   }
+  
 }
 
 main();
+
+app.use(async ctx => {
+  ctx.body = await main();
+});
+
+app.listen(3000);
+
